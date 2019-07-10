0d11f33cdfe3a2d8c8d13b075264c876477acc0b
Andrew Krause <andrew.l.krause@gmail.com>
2019-07-09 22:23:53 -0500

    working on BasicForm

--- src/layouts/forms/BasicForm/index.js
+++ src/layouts/forms/BasicForm/index.js
@@ -1,20 +1,12 @@
 import React  from 'react'; 
-import { makeStyles } from '@material-ui/core/styles'; 
+
+// @material-ui  
+import Icon from '@material-ui/core/Icon'; 
 import TextField from '@material-ui/core/TextField'; 
 import Button from '@material-ui/core/Button'; 
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1), 
-  }, 
-  input: {
-    display: 'none', 
-  },
-}));
-
 
 class BasicForm extends React.Component {
-    const classes = useStyles(); 
 
   constructor(props) {
     super(props);
@@ -41,31 +33,10 @@ class BasicForm extends React.Component {
           Name:
           <TextField type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
-        <Button variant='raised' color="primary" type="submit">Submit</Button>
+        <Button variant='contained' color="primary" type="submit">Submit</Button>
       </form>
     );
   }
 }
 
-export default function BasicForm() { 
-  const classes = useStyles(); 
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  return ( 
-    <form onSubmit={this.handleSubmit}>
-      <label>
-        Name:
-        <TextField type="text" value={this.state.value} onChange={this.handleChange} />
-      </label>
-      <Button variant='contained' color="primary" className={classes.button} type="submit">Submit</Button>
-    </form>
-  )
-}
+export default BasicForm;
